b36e1b341c8580cac8b8f493673392daa7ec8f6c
samod singh <samod0098@gmail.com>
2021-08-17 13:59:13 +0530

    Udemt-3

---
+++ script.js
@@ -0,0 +1,24 @@
+let age ="18";
+if(age===18)
+console.log("You just become an adult:d (strict)s");
+
+if (age ==18)
+console.log("You just become an adult:D (lose)");
+
+
+let fevourite = prompt ("what's your favourite number ?");
+console.log(fevourite);
+
+console.log(typeof fevourite);
+if(fevourite === 23){
+    console.log("Cool! 23 is an amzaing number!");
+}
+else if(fevourite ===7){
+    console.log("7 is also a cool number");
+}
+else if(fevourite === 9){
+    console.log("9 is also cool number")
+}
+else{
+    console.log("Number is not 23 or 7 or 9")
+}
